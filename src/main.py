9e538f38de1fbaf286fc5166568508022351dbb9
zonetecde <zonetecde@gmail.com>
2023-10-18 14:55:57 +0200

    init du code python + ouverte de la page web + communication entre js et py

---
+++ src/main.py
@@ -0,0 +1,14 @@
+import webview
+
+from api.api import Api
+    
+def main():
+    # Instancie l'Api pour communiquer avec la page web
+    api = Api()
+    window = webview.create_window('Nom du projet', './web/index.html', js_api=api)
+    api.set_window(window) 
+
+    webview.start() 
+
+if __name__ == "__main__":
+         main()
