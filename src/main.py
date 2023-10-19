8bc7a96b2d02a0154bef07885f0ba3cd6d201019
Myumiki <wolf.esteblue@gmail.com>
2023-10-19 13:16:20 +0200

    init esteban

--- src/main.py
+++ src/main.py
@@ -6,9 +6,9 @@ def main():
     # Instancie l'Api pour communiquer avec la page web
     api = Api()
     window = webview.create_window('Nom du projet', './web/index.html', js_api=api)
-    api.set_window(window) 
+    api.set_window(window)  
 
     webview.start() 
 
 if __name__ == "__main__":
-         main()
+        main()
