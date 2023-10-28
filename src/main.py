f27e5dd99e14ab08d6fb1b14a50d1c3308aa4eeb
Rayane Staszewski <zonetecde@gmail.com>
2023-10-28 17:25:43 +0200

    init web

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ from api.api import Api
 def main():
     # Instancie l'Api pour communiquer avec la page web
     api = Api()
-    window = webview.create_window('Nom du projet', './web/index.html', js_api=api, resizable=True, min_size=(800, 600), width=1080, height=720)
+    window = webview.create_window('Nom du projet', './web/build/index.html', js_api=api, resizable=True, min_size=(800, 600), width=1080, height=720)
     api.set_window(window) 
 
     webview.start() 
---
+++ src/web/src/routes/+error.svelte
@@ -0,0 +1,6 @@
+<script>
+    import { page } from '$app/stores';
+    var currentUrl = window.location.href;
+  </script>
+  
+<h1>{$page.status}: {$page.error.message} {currentUrl}</h1>
