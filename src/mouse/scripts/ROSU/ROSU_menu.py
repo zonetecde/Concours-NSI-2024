4c16cd7e168ef7145c74fe2dbfdf3bbf5cb004d9
Lilifyfy55555 <lilian57535gmail.com>
2024-02-14 15:02:13 +0100

    Upd ROSU

--- src/mouse/scripts/ROSU/ROSU_menu.py
+++ src/mouse/scripts/ROSU/ROSU_menu.py
@@ -1,6 +1,20 @@
 import pygame
 import sys
 import math
+import json
+from os.path import exists
+
+if not exists("src/mouse/scripts/ROSU/savefile.json"):
+    with open("src/mouse/scripts/ROSU/savefile.json", "w") as f:
+        json.dump(
+            { "Gravity Falls" : { "Best Score" : "-----", "Grade" : "None", "Accuracy" : "None"}, "Okami" : { "Best Score" : "-----", "Grade" : "None", "Accuracy" : "None"}, "Zelda -- Hidden Village" : { "Best Score" : "-----", "Grade" : "None", "Accuracy" : "None"}}
+            , f)
+        
+savefile = open("src/mouse/scripts/ROSU/savefile.json")
+tempData = json.load(savefile)
+data = []
+for save in tempData:
+    data.append((save, tempData[save]))
 
 import ROSU_storage as storage
 
@@ -24,6 +38,7 @@ rectZones = []
 
 running = True
 bgImage = None
+selected = ""
 
 playSong = None
 while running:
@@ -47,9 +62,38 @@ while running:
         
         if mouseX > x and mouseX < x + 600 and mouseY > y and mouseY < y + 150:
             bgImage = pygame.image.load(storage.Storage[i][2])
+            selected = storage.Storage[i][0]
             
-        rectZones.append((i, x, y, x + 600, y + 150))  
+        rectZones.append((i, x, y, x + 600, y + 150)) 
         y += 160
+    
+    obj = 0
+    for item in data:
+        if str(item[0]) == str(selected):
+            songName = str(item[0])
+            saveDict = data[obj][1]
+            bestScore = saveDict["Best Score"]
+            grade = saveDict["Grade"]
+            accuracy = saveDict["Accuracy"]
+            
+            
+            textFont = pygame.font.SysFont("monospace", 35, bold=True, italic=False)
+            nameLabel = textFont.render((songName), 1, (255, 255, 255))
+            screen.blit(nameLabel, (650, 10))
+            
+            textFont = pygame.font.SysFont("monospace", 35, bold=True, italic=False)
+            scoreLabel = textFont.render(("Best score: " + str(bestScore)), 1, (255, 255, 255))
+            screen.blit(scoreLabel, (650, 120))
+            
+            textFont = pygame.font.SysFont("monospace", 35, bold=True, italic=False)
+            scoreLabel = textFont.render(("Accuracy: " + str(accuracy)), 1, (255, 255, 255))
+            screen.blit(scoreLabel, (650, 180))
+            
+            textFont = pygame.font.SysFont("monospace", 35, bold=True, italic=False)
+            scoreLabel = textFont.render(("Grade: " + str(grade)), 1, (255, 255, 255))
+            screen.blit(scoreLabel, (650, 240))
+            
+        obj += 1
         
         
     
@@ -61,10 +105,13 @@ while running:
             mouseY = pygame.mouse.get_pos()[1]
             for j in range(len(rectZones)):
                 if mouseX > rectZones[j][1] and mouseX < rectZones[j][3] and mouseY > rectZones[j][2] and mouseY < rectZones[j][4]:
-                    print("in " + str(j)) 
                     import ROSU_game as game
                     game.GAMELOOP(j)
-                    
+                    savefile = open("src/mouse/scripts/ROSU/savefile.json")
+                    tempData = json.load(savefile)
+                    data = []
+                    for save in tempData:
+                        data.append((save, tempData[save]))
         
     # Update the display
     pygame.display.flip()
