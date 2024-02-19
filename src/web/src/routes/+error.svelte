d3c9d08704b1150f373436dacb12f39fb045bb5c
zonetecde <zonetecde@gmail.com>
2024-02-19 15:32:28 +0100

    + page erreur qui redirect

---
+++ src/web/src/routes/+error.svelte
@@ -0,0 +1,12 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		// Simule un clique sur un link <a> pour aller à la racine du site
+		document.querySelector('a')?.click();
+	});
+</script>
+
+<div class="flex items-center justify-center">
+	<a href="/" class="text-4xl font-bold">Retour à l'accueil</a>
+</div>
