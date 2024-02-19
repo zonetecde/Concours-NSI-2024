dcca0013d3b181f3ec538fa0764ff9afa85044e1
zonetecde <zonetecde@gmail.com>
2024-02-19 15:39:36 +0100

    + t

--- src/web/src/routes/+error.svelte
+++ src/web/src/routes/+error.svelte
@@ -1,12 +1,15 @@
 <script>
 	import { onMount } from 'svelte';
+	import { page } from '$app/stores';
 
 	onMount(() => {
 		// Simule un clique sur un link <a> pour aller à la racine du site
-		document.querySelector('a')?.click();
+		//document.querySelector('a')?.click();
 	});
 </script>
 
-<div class="flex items-center justify-center">
+<div class="flex flex-col items-center justify-center">
+	<h1>{$page.status}: {$page.error?.message}</h1>
+
 	<a href="/" class="text-4xl font-bold">Retour à l'accueil</a>
 </div>
