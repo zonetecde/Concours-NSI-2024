ffe2e015bfa53db02f9149b01ac97aabf2a91d87
zonetecde <zonetecde@gmail.com>
2024-03-03 22:07:35 +0100

    + stenographie python fonctionnel

--- sources/keyboard/stenographie/stenographie.py
+++ sources/keyboard/stenographie/stenographie.py
@@ -1,6 +1,11 @@
+import os
 import random
-import urllib3
+import re
+import urllib.request
 
+import requests
+import sys
+import tarfile
 
 class Stenographie:
     def get_audios_with_texts(self):
@@ -14,7 +19,6 @@ class Stenographie:
         # à https://www.voxforge.org/home/downloads/speech/french-speech-files?pn=76 (inclus)
 
         # Prend une page aléatoire entre 1 et 76 au format https://www.voxforge.org/home/downloads/speech/french-speech-files?pn=LE NUMERO DE LA PAGE
-
         page_url = self.get_random_voxforge_page_url()
 
         # Récupère le contenu de la page
@@ -26,27 +30,39 @@ class Stenographie:
 
         random_link = random.choice(links) # lien aléatoire
 
-        # De ce lien, récupère le fichier .tgz (voir sur le site oú il est situé)
+        # De ce lien, récupère le fichier .tgz 
         tgz_download_link = self.get_tgz_download_link(random_link)
 
         # Télécharge le fichier .tgz dans le dossier audio 
-        self.download_tgz(tgz_download_link)
+        filepath = self.download_tgz(tgz_download_link)
 
         # décompresse le fichier .tgz
         # dans le fichier tgz se trouve un autre fichier .tar, il faut aussi le décompresser
-        self.decompress_tgz("audio/le_fichier.tgz")
-        self.decompress_tar("audio/le_dossier_tgz_decompresse/le_fichier.tar")
+        folder_path = self.extract_tgz(filepath, os.path.dirname(filepath))
 
         # dans le fichier tar se trouve un dossier contenant les audios et un fichier texte avec la retranscription de l'audio
         # les fichiers audios se trouvent dans le dossier wav et les retrancriptions dans le dossier etc/PROMPTS
         # Récupère le texte et le chemin d'accès vers l'audio et les met dans une liste de tuple
+        audios = []
+        text_file = os.path.join(folder_path, "etc/PROMPTS")
+
+        with open(text_file, "r", encoding="UTF-8") as file:
+            for line in file:
+                # Le chemin d'accès vers l'audio est entre le début et le premier espace
+                data = line.split(" ", maxsplit=1)
+                audio_path = os.path.join(os.path.dirname(folder_path), data[0])
+                text = data[1].capitalize().replace("\n", "")
+                audios.append((text, audio_path))
 
         # renvoie la liste de tuple (format : [(texte, chemin_audio), (texte, chemin_audio), ...])
+        return audios
     
     def get_html(self, url):
         """Télécharge le contenu d'une page web et le retourne sous forme de texte.
         """
-        fp = urllib3.request.urlopen(url)
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        req = urllib.request.Request(url, headers=headers)
+        fp = urllib.request.urlopen(req)
         bytes = fp.read()
 
         html = bytes.decode("utf8")
@@ -58,13 +74,20 @@ class Stenographie:
         """Renvoie une page aléatoire de voxforge.org
         Entre 1 et 76
         """
-        pass
+        random_page = random.randint(1, 76)
+        return f'https://www.voxforge.org/home/downloads/speech/french-speech-files?pn={random_page}'
 
     def get_links_in_voxforge_page(self, html):
         """Récupère les liens dans une page de voxforge.org
-
         """
-        pass
+        # Entre <td class="oddThread"><a href=" et ">
+        links = re.findall(r'<td class="oddThread"><a href="(.*?)">', html)
+        # Enlève les liens qui amènent vers un profile
+        links = list(filter(lambda link: "viewProfile" not in link, links))
+        # Ajoute au début des liens la source du site (https://www.voxforge.org/)
+        links = [f"https://www.voxforge.org/{link}" for link in links]
+
+        return links
 
     def get_tgz_download_link(self, url):
         """Récupère le lien de téléchargement du .tgz sur une page voxforge.org
@@ -73,32 +96,49 @@ class Stenographie:
             url (str): url de la page (ex: https://www.voxforge.org/home/downloads/speech/french-speech-files/nbara-20160203-pkd#zMNM6jv5nOFCo0Wz1eM_0Q)
         """
         html = self.get_html(url)
-        pass
+        # Le lien se trouve entre /compressed.gif"><a href=" et ">
+        link = re.findall(r'/compressed.gif"><a href="(.*?)">', html)[0]
+        return link
 
     def download_tgz(self, url):
         """Télécharge un fichier .tgz depuis une url
 
         Args:
             url (str): url du fichier .tgz
-        """
-        pass
 
-    def decompress_tgz(self, path):
+        Returns:
+            (str): Le path vers le fichier téléchargé
+        """
+        dir = os.path.dirname(os.path.abspath(__file__)) + "/audios/"
+
+        headers = {'User-Agent': 'Mozilla/5.0'}
+        r = requests.get(url, allow_redirects=True, headers=headers)
+        
+        # Nom de fichier aléatoire
+        filepath = str(random.randint(0, 999_999)) + ".tgz"
+
+        # Sauvegarde le fichier sur le disque
+        open(dir + filepath, 'wb').write(r.content)
+        
+        return dir + filepath
+
+    def extract_tgz(self, filepath, where):
         """Décompresse un fichier .tgz
 
         Args:
             path (str): chemin d'accès vers le fichier .tgz
+
+        Returns:
+            (str): Le nom du dossier où le contenu a été extrait
         """
-        pass
+        tar = tarfile.open(filepath, 'r')
+        folder_name = ""
 
-    def decompress_tar(self, path):
-        """Décompresse un fichier .tar
+        for item in tar:
+            tar.extract(item, where)
+            folder_name = item.name
 
-        Args:
-            path (str): chemin d'accès vers le fichier .tar
-        """
-        pass
+        return os.path.join(where, os.path.dirname(os.path.dirname(folder_name))) 
 
-
-stenographie = Stenographie()
-print(stenographie.get_audios_with_texts())
+# stenographie = Stenographie()
+# print(stenographie.get_audios_with_texts())
