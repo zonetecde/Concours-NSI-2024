37a2e5aacfb1aaba579ac9e9a0c661de54a7670e
zonetecde <zonetecde@gmail.com>
2024-03-05 11:55:45 +0100

    + py rythme commencement

--- sources/web/src/routes/+page.svelte
+++ sources/web/src/routes/+page.svelte
@@ -35,14 +35,14 @@
 	/* Style du fond de la fenêtre */
 	#main {
 		--s: 200px;
-		--c: #bcd5f7;
+		--c: #9cb7dd;
 
 		--_g: #0000 8%, var(--c) 0 17%, #0000 0 58%;
 		background: linear-gradient(135deg, #0000 20.5%, var(--c) 0 29.5%, #0000 0) 0 calc(var(--s) / 4),
 			linear-gradient(45deg, var(--_g)) calc(var(--s) / 2) 0,
 			linear-gradient(135deg, var(--_g), var(--c) 0 67%, #0000 0),
 			linear-gradient(45deg, var(--_g), var(--c) 0 67%, #0000 0 83%, var(--c) 0 92%, #0000 0),
-			#a9c7e4;
+			#9dbedd;
 		background-size: var(--s) var(--s);
 	}
 </style>
