1be810f37fe0ed314fe6fe5d1bbdf3e8979c81f5
zonetecde <zonetecde@gmail.com>
2024-03-18 14:25:45 +0100

    + phrase explicative

--- sources/web/src/lib/Exercice.svelte
+++ sources/web/src/lib/Exercice.svelte
@@ -27,12 +27,10 @@
 	href={link || ''}
 	class="flex flex-col text-center gap-y-3 hover:scale-110 duration-150 xl:h-56 xl:w-80 md:w-52 md:h-40 h-28 w-40 items-center justify-center text-white bg-[#00000017] border-2 border-[#3b3b3b21] shadow-md rounded-md p-2 cursor-pointer hover:shadow-lg transition-all"
 	on:click={handleExerciceClicked}
+	on:mouseenter
+	on:mouseleave
 >
-	<img
-		src={image}
-		alt="img de l'exercice"
-		class={'bg-[#3b3b3b21] w-full h-[70%] mt-2 ' + (imgStyle || 'object-cover')}
-	/>
+	<img src={image} alt="img de l'exercice" class={'bg-[#3b3b3b21] w-full h-[70%] mt-2 ' + (imgStyle || 'object-cover')} />
 
 	<p class="text-lg">{nom}</p>
 </a>
--- sources/web/src/routes/keyboard/+page.svelte
+++ sources/web/src/routes/keyboard/+page.svelte
@@ -1,18 +1,52 @@
 <script>
 	import Exercice from '$lib/Exercice.svelte';
 	import Retour from '$lib/Retour.svelte';
+
+	let phraseExplicatique = '';
 </script>
 
 <div class="flex pt-10 h-screen justify-center flex-col items-center w-screen">
 	<h1 class="font-bold text-4xl -mt-12">Entrainement au clavier</h1>
 
 	<div class="flex gap-x-8 gap-y-10 items-center justify-center flex-wrap mt-10 w-[90%]">
-		<Exercice image="/keyboard/typescript.jpg" link="/keyboard/type-script" nom="Type Script" />
-		<Exercice image="/keyboard/rythme.jpg" nom="Rythme" link="/keyboard/rythme" />
-		<Exercice image="/keyboard/reaction.jpg" nom="Reaction" link="/keyboard/reaction" />
-		<Exercice image="/keyboard/bac.jpg" nom="Jeu du Bac" link="/keyboard/bac" />
-		<Exercice image="/keyboard/stenographie.jpg" nom="Sténographie" link="/keyboard/stenographie" />
+		<Exercice
+			image="/keyboard/typescript.jpg"
+			link="/keyboard/type-script"
+			nom="Type Script"
+			on:mouseleave={() => (phraseExplicatique = '')}
+			on:mouseenter={() => (phraseExplicatique = 'Apprenez à taper plus vite et à mieux connaitre votre clavier')}
+		/>
+		<Exercice
+			image="/keyboard/rythme.jpg"
+			nom="Rythme"
+			link="/keyboard/rythme"
+			on:mouseleave={() => (phraseExplicatique = '')}
+			on:mouseenter={() => (phraseExplicatique = 'Dans le style de Guitar Hero, améliorer votre coordination en tapant au rythme de la musique')}
+		/>
+		<Exercice
+			image="/keyboard/reaction.jpg"
+			nom="Reaction"
+			link="/keyboard/reaction"
+			on:mouseleave={() => (phraseExplicatique = '')}
+			on:mouseenter={() => (phraseExplicatique = 'Améliorer votre temps de réaction en écrivant des caractères le plus vite possible')}
+		/>
+		<Exercice
+			image="/keyboard/bac.jpg"
+			nom="Jeu du Bac"
+			link="/keyboard/bac"
+			on:mouseleave={() => (phraseExplicatique = '')}
+			on:mouseenter={() => (phraseExplicatique = 'Jeu de culture générale, taper de vous-même vos réponses aux thèmes sélectionnés')}
+		/>
+		<Exercice
+			image="/keyboard/stenographie.jpg"
+			nom="Sténographie"
+			link="/keyboard/stenographie"
+			on:mouseleave={() => (phraseExplicatique = '')}
+			on:mouseenter={() => (phraseExplicatique = 'Ecrivez le script entendu le plus vite possible et le plus précisément possible')}
+		/>
 	</div>
+
+	<p class="text-center text-base lg:bottom-10 bottom-5 lg:text-xl absolute left-1/2 -translate-x-1/2">{phraseExplicatique}</p>
 
 	<Retour urlToGo="/" />
 </div>
